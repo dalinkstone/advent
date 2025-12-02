150533e121f28543f9be258b57913db3d8b2d540
dalinkstone <dalinkstone@gmail.com>
2025-12-01 23:05:21 -0700

    this is just setting up the structure of the file for solving later

---
+++ day2/decipher.py
@@ -0,0 +1,9 @@
+def decipher(filename: str) -> bool:
+
+    with open(filename, "r") as file:
+        for line in file:
+            print(line)
+    return False
+
+if __name__ == "__main__":
+    decipher("input.txt")
