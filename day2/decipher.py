8a962828302e88e3dfceece495427c527d7e70ae
dalinkstone <dalinkstone@gmail.com>
2025-12-03 00:25:02 -0700

    finished day 2

--- day2/decipher.py
+++ day2/decipher.py
@@ -9,10 +9,37 @@ def decipher(filename: str) -> bool:
             for item in lineResult:
                 rangeResult = item.split("-")
                 
-                searchRange = range(int(rangeResult[0]), int(rangeResult[1]))
+                searchRange = range(int(rangeResult[0]), int(rangeResult[1])+1)
 
-                
+                print(f"This is the current search range {searchRange}")
 
+                for num in searchRange:
+
+                    strNum = str(num)
+
+                    lenNum = len(strNum)
+
+                    if lenNum == 1:
+                        continue
+
+                    for patternLen in range(1, (lenNum // 2) + 1):
+                        
+                        if lenNum % patternLen == 0:
+                            pattern = strNum[:patternLen]
+
+                            possibleMatch = pattern * (lenNum // patternLen)
+
+                            if strNum == possibleMatch:
+                                print(f"This is the match {strNum}")
+
+                                invalidIdResult += num
+                                break
+
+
+
+
+    print(f"The new count is {invalidIdResult}")
+    
     return False
 
 if __name__ == "__main__":
