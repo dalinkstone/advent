49b50f4cee072bb42c8a9fdedf48e0eda486bdc8
dalinkstone <dalinkstone@gmail.com>
2025-12-07 22:39:07 -0700

    this is the basic structure of the file with the euclidean distance formula for two points

--- day8/decipher.py
+++ day8/decipher.py
@@ -1,7 +1,22 @@
-count = 0
-
 def decipher(filename: str) -> int:
+    with open(filename, "r") as file:
+        rows = [[int(num) for num in line.strip().split(",")] for line in file] 
+        
+    # euclidean_distance = square_root(square(x1-x2)+square(y1-y2)+square(z1-z2))
 
+    print(rows[0])
+    print(rows[1])
+
+    num = 0
+
+    for i in range(3):
+        num += (rows[0][i] - rows[1][i]) ** 2
+
+    num = num ** 0.5
+
+    print(num)
+
+    count = 0
     return count
 
 if __name__ == '__main__':
