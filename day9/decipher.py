c28a002a28313c44a7e777d00f19c9f632cfb514
dalinkstone <dalinkstone@gmail.com>
2025-12-09 18:11:30 -0700

    this completes day 9 part 1 using inclusive area

--- day9/decipher.py
+++ day9/decipher.py
@@ -1,9 +1,17 @@
 def decipher(filename: str) -> int:
     count = 0
+    coords = []
+    
 
     with open(filename, "r") as file:
         for line in file:
-            print(line)
+            coords.append([int(num) for num in line.strip().split(",")])
+
+    for i in range(len(coords)):
+
+        for j in range(i + 1, len(coords)):
+
+            count = max(count, (abs(coords[i][0] - coords[j][0]) + 1) * (abs(coords[i][1] - coords[j][1]) + 1))
 
     return count
 
